54ad06a191f70734cb84fa30bd16ebea2519b039
Alfred <alfredrajojot88@gmail.com>
2024-05-17 19:48:18 +0700

    testing

--- app/src/main/java/com/example/savvyswantatra/MainActivity.kt
+++ app/src/main/java/com/example/savvyswantatra/MainActivity.kt
@@ -22,7 +22,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            Prelogin()
+            SavvySwantatraTheme {
+                Prelogin()
+            }
         }
     }
 }
--- app/src/main/java/com/example/savvyswantatra/navigation/Screens.kt
+++ app/src/main/java/com/example/savvyswantatra/navigation/Screens.kt
@@ -1,18 +1,18 @@
-package com.example.savvyswantatra.navigation
+    package com.example.savvyswantatra.navigation
 
-enum class Screens {
-    PreLogin,
-    Login,
-    Register,
-    Verification,
-    PhoneVerification,
-    OTPVerification,
-    Home,
-    Calendar,
-    Budget,
-    Saving,
-    Settings,
-    Profile,
-    Password,
-    Agreement
-}
+    enum class Screens {
+        PreLogin,
+        Login,
+        Register,
+        Verification,
+        PhoneVerification,
+        OTPVerification,
+        Home,
+        Calendar,
+        Budget,
+        Saving,
+        Settings,
+        Profile,
+        Password,
+        Agreement
+    }
