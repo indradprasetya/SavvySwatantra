5395668347cfeefc4e2e8cc6c647371a8801cf4c
indradprasetya <indrayanadian7@gmail.com>
2024-05-18 21:07:49 +0700

    add new file and modify Verif.kt

--- app/src/main/java/com/example/savvyswantatra/register/Verif.kt
+++ app/src/main/java/com/example/savvyswantatra/register/Verif.kt
@@ -1,67 +1,75 @@
 package com.example.savvyswantatra.register
 
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.savvyswantatra.R
 import com.example.savvyswantatra.ui.theme.OrangeSavvy
+import com.example.savvyswantatra.ui.theme.PinkSavvy
 import com.example.savvyswantatra.ui.theme.PurpleSavvy1
 import com.example.savvyswantatra.ui.theme.PurpleSavvy2
 import com.example.savvyswantatra.ui.theme.Typography
+import com.example.savvyswantatra.ui.theme.WhiteSavvy
 import com.example.savvyswantatra.ui.theme.poppinsFontFamily
 
 @Composable
 fun Verif() {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(PurpleSavvy1)
     ) {
 //        Title
-        Text(text = "Selamat Datang!", style = Typography.titleLarge, color = PurpleSavvy1)
+        Spacer(modifier = Modifier.height(100.dp))
+        Text(text = "Verifikasi Akun Anda", style = Typography.titleLarge, color = WhiteSavvy)
         Text(
-            text = "Sikahkan  isi form dibawah ini untuk \n membuat akun savvy anda",
+            text = "Silahkan verifikasi akun Anda terlebih dahulu,\nsebelum masuk ke dalam aplikasi ",
             style = Typography.labelSmall,
             textAlign = TextAlign.Center,
-            color = PurpleSavvy2
+            color = PinkSavvy
         )
-        Spacer(modifier = Modifier.height(100.dp))
-//        Form
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Nama", fontFamily = poppinsFontFamily) })
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Email", fontFamily = poppinsFontFamily) })
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Password", fontFamily = poppinsFontFamily) })
-
-        OutlinedTextField(
-            value = "",
-            onValueChange = {},
-            label = { Text("Ulangi Password", fontFamily = poppinsFontFamily) })
-        Spacer(modifier = Modifier.height(100.dp))
+//        Image
+        Image(
+            painter = painterResource(id = R.drawable.verif),
+            contentDescription = "verif",
+            modifier = Modifier
+                .size(258.dp)
+                .offset(y = 50.dp)
+        )
 //        Button
         Button(
             onClick = { /*TODO*/ },
-            colors = androidx.compose.material3.ButtonDefaults.buttonColors(
-                containerColor = OrangeSavvy,
-            ),
+            colors = buttonColors(containerColor = OrangeSavvy),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .size(width = 186.dp, height = 46.dp)
+                .offset(y = 220.dp)
         ) {
             Text(text = "Lanjut", style = Typography.displayMedium)
         }
