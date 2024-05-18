6735e229b3049bd187808c1b5f34a968687133ba
indradprasetya <indrayanadian7@gmail.com>
2024-05-18 21:31:15 +0700

    add new OtpPhoneNumber

--- app/src/main/java/com/example/savvyswantatra/register/Verif.kt
+++ app/src/main/java/com/example/savvyswantatra/register/Verif.kt
@@ -15,15 +15,18 @@ import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -73,6 +76,45 @@ fun Verif() {
         ) {
             Text(text = "Lanjut", style = Typography.displayMedium)
         }
+    }
+}
+
+@Composable
+fun OtpPhoneNumber() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(PurpleSavvy1)
+    ) {
+//        Title
+        Spacer(modifier = Modifier.height(100.dp))
+        Text(text = "Masukkan Nomor HP", style = Typography.titleLarge, color = WhiteSavvy)
+        Text(
+            text = "Masukkan No.HP Anda untuk menerima\nkode OTP",
+            style = Typography.labelSmall,
+            textAlign = TextAlign.Center,
+            color = PinkSavvy
+        )
+//        Form
+        TextField(
+            value = "",
+            onValueChange = {},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.offset(y = 30.dp)
+            )
+//        Button
+            Button(
+                onClick = { /*TODO*/ },
+                colors = buttonColors(containerColor = OrangeSavvy),
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier
+                    .size(width = 186.dp, height = 46.dp)
+                    .offset(y = 420.dp)
+            ) {
+                Text(text = "Kirim Kode OTP", style = Typography.displayMedium)
+            }
     }
 }
 
