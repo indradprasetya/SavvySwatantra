25ee2797a24ea84f539aee964a91f0267967845b
IndraPrasetya <indrayanadian7@gmail.com>
2024-05-21 21:55:02 +0700

    resolve conflict

--- app/src/main/java/com/example/savvyswantatra/navigation/Screens.kt
+++ app/src/main/java/com/example/savvyswantatra/navigation/Screens.kt
@@ -1,18 +1,31 @@
-package com.example.savvyswantatra.navigation
+    package com.example.savvyswantatra.navigation
 
-enum class Screens {
-    PreLogin,
-    Login,
-    Register,
-    Verif,
-    OtpPhoneNumber,
-    OTPCode,
-    Home,
-    Calendar,
-    Budget,
-    Saving,
-    Settings,
-    Profile,
-    Password,
-    Agreement
-}
+
+
+    sealed class  Screen(val route : String){
+        data object Splash : Screen("splash")
+        data object preLogin1 : Screen("preLogin1")
+        data object preLogin2 : Screen("preLogin2")
+        data object preLogin3 : Screen("preLogin3")
+        data object beranda : Screen("beranda")
+        data object kalender : Screen("kalender")
+        data object anggaran : Screen("anggaran")
+        data object simpanan : Screen("simpanan")
+        data object pengaturan : Screen("pengaturan")
+    }
+//    enum class Screens {
+//        PreLogin,
+//        Login,
+//        Register,
+//        Verification,
+//        PhoneVerification,
+//        OTPVerification,
+//        Home,
+//        Calendar,
+//        Budget,
+//        Saving,
+//        Settings,
+//        Profile,
+//        Password,
+//        Agreement
+//    }
