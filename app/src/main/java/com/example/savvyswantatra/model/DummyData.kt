c85db9cee0afc94a441fed9f374700ac7a44f994
TirtaAbdul <tirtaabdulh@gmail.com>
2024-06-19 15:10:35 +0700

    DummyData terbaru, Kalender Bar terbaru

---
+++ app/src/main/java/com/example/savvyswantatra/model/DummyData.kt
@@ -0,0 +1,119 @@
+package com.example.savvyswantatra.model
+
+import com.example.savvyswantatra.R
+import com.example.savvyswantatra.model.KalenderData
+import androidx.compose.runtime.mutableStateListOf
+
+object DummyData {
+    val KalenderList = listOf(
+        KalenderData(
+            id = 1,
+            image = R.drawable.makan,
+            icon = R.drawable.pengeluaran,
+            kategori = "Makanan dan Minuman",
+            deskripsi = "Makan Warteg",
+            harga = "Rp10,000"
+        ),
+        KalenderData(
+            id = 2,
+            image = R.drawable.kesehatanicon,
+            icon = R.drawable.pengeluaran,
+            kategori = "Kesehatan",
+            deskripsi = "Panadol",
+            harga = "Rp15,000"
+        ),
+        KalenderData(
+            id = 3,
+            image = R.drawable.income,
+            icon = R.drawable.pemasukan,
+            kategori = "Mandiri",
+            deskripsi = "Transfer dari sepuh alfred",
+            harga = "Rp12,000"
+        ),
+        KalenderData(
+            id = 4,
+            image = R.drawable.income,
+            icon = R.drawable.pemasukan,
+            kategori = "Mandiri",
+            deskripsi = "Thr dari paman",
+            harga = "Rp12,000"
+        ),
+        KalenderData(
+            id = 5,
+            image = R.drawable.income,
+            icon = R.drawable.pemasukan,
+            kategori = "Mandiri",
+            deskripsi = "Thr dari paman",
+            harga = "Rp12,000"
+        ),
+        KalenderData(
+            id = 6,
+            image = R.drawable.income,
+            icon = R.drawable.pemasukan,
+            kategori = "Mandiri",
+            deskripsi = "Thr dari paman",
+            harga = "Rp12,000"
+        ),
+        KalenderData(
+            id = 7,
+        image = R.drawable.income,
+        icon = R.drawable.pemasukan,
+        kategori = "Mandiri",
+        deskripsi = "Thr dari paman",
+        harga = "Rp12,000"
+    )
+    )
+}
+
+object DummyData2 {
+    val TanggalList = listOf(
+        TanggalitemData(
+            date = "2024-06-15",
+            id = 1,
+            image = R.drawable.makan,
+            icon = R.drawable.pengeluaran,
+            kategori = "Makanan dan Minuman",
+            deskripsi = "Makan Warteg",
+            harga = "Rp10,000"
+        ),
+        TanggalitemData(
+            date = "2024-06-15",
+            id = 2,
+            image = R.drawable.kesehatanicon,
+            icon = R.drawable.pengeluaran,
+            kategori = "Kesehatan",
+            deskripsi = "Panadol",
+            harga = "Rp15,000"
+        ),
+        TanggalitemData(
+            date = "2024-06-16",
+            id = 3,
+            image = R.drawable.income,
+            icon = R.drawable.pemasukan,
+            kategori = "Mandiri",
+            deskripsi = "Transfer dari sepuh alfred",
+            harga = "Rp12,000"
+        ),
+        TanggalitemData(
+            date = "2024=06-17",
+            id = 4,
+            image = R.drawable.income,
+            icon = R.drawable.pemasukan,
+            kategori = "Mandiri",
+            deskripsi = "Thr dari paman",
+            harga = "Rp12,000"
+        ),
+        TanggalitemData(
+            date = "2024-06-17",
+            id = 5,
+            image = R.drawable.income,
+            icon = R.drawable.pemasukan,
+            kategori = "Mandiri",
+            deskripsi = "Thr dari paman",
+            harga = "Rp12,000"
+        ),
+
+    )
+}
+
+
